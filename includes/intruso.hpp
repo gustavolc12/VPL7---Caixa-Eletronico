32006e1a8b2bf113a930d0f6730b19fdaf902f13
gustavolc12 <gustavolc09@gmail.com>
2022-10-22 17:01:14 -0300

    Map

--- includes/intruso.hpp
+++ includes/intruso.hpp
@@ -1,7 +1,10 @@
 #include <string>
+#include <vector>
+#include <map>
 
 class Intruso{
     /*Continue a implementação da classe Intruso*/
+    std::vector<std::pair<std::map<char, std::pair<int, int>>, std::vector<char>>;
 
     public:
         void set_senha_vazada(std::string vazou);
