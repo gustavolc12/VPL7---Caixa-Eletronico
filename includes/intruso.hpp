364aa097ede77a02e33a7d1c41eee64287a6319c
gustavolc12 <gustavolc09@gmail.com>
2022-10-22 17:06:03 -0300

    Nome

--- includes/intruso.hpp
+++ includes/intruso.hpp
@@ -1,10 +1,11 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <utility>
 
 class Intruso{
     /*Continue a implementação da classe Intruso*/
-    std::vector<std::pair<std::map<char, std::pair<int, int>>, std::vector<char>>;
+    std::vector<std::pair<std::map<char, std::pair<int, int>>, std::vector<char>>> armazenamento;
 
     public:
         void set_senha_vazada(std::string vazou);
