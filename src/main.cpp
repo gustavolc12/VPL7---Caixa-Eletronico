6f195b61a362c542a13e49877c8dbf07341c72c6
matbs <mateusbs20222@gmail.com>
2022-10-23 17:15:22 -0300

    alteração IF

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,10 @@ int main(){
     std::cin >> num_entradas;
 
     for(i=0; i < num_entradas; i++){
+        if(i==0){
         std::cin.ignore();
-        std::getline(std::cin, senha);
+        }
+        std::getline(std::cin, senha);// Nesse getline quando a iteração passa pela segunda vez o primeiro elemento é ignorado, começando por um elemento a
         intruso.set_senha_vazada(senha);
     }
     std::cout << intruso.crack_senha() <<std::endl;
