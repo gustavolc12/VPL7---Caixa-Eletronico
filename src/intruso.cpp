ae43d34da6e2f5fa4ac5339ad91644577b8bd48a
gustavolc12 <gustavolc09@gmail.com>
2022-10-23 14:06:26 -0300

    fUNÇÕES

--- src/intruso.cpp
+++ src/intruso.cpp
@@ -13,9 +13,27 @@ void Intruso::set_senha_vazada(std::string vazou){
 }
 std::string Intruso::crack_senha(){
     std::vector<std::vector<std::pair<int, int>>> senha;
+    std::vector<int> resultado;
     for(int i=0; i<armazenamento.size(); i++){
         for(int j=0; j<6; j++){
             senha[i][j]= (armazenamento[i]).first[(armazenamento[i]).second[j]];
+        }
+    }
+    for(int i=0; i<6; i++){
+        bool verifica = true;
+        for(int j=0; j<armazenamento.size()-1; j++){
+            if(senha[1][i].first == senha[j+1][i].first || senha[j][i].first == senha[j+1][i].second){
+                
+            }
+            else{
+                verifica = false;
+                break;
+            }
+        }
+        if(verifica){
+            resultado[i]=senha[1][i].first;
+        }else{
+            resultado[i]=senha[1][i].second;
         }
     }
     
