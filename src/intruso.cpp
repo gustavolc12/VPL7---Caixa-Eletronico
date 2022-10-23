9ca621c49f9ca71336d9be351652c5aa737c71df
gustavolc12 <gustavolc09@gmail.com>
2022-10-23 10:01:57 -0300

    senha

--- src/intruso.cpp
+++ src/intruso.cpp
@@ -1,6 +1,13 @@
 #include "../includes/intruso.hpp"
 void Intruso::set_senha_vazada(std::string vazou){
-
-    std::getline(vazou, ' ');
+    std::map<char, std::pair<int, int>> codigo;
+    std::vector<char> senha;
+    for(int i=0;i<5;i++){
+        codigo[char('A'+i)] = std::pair<int, int> ((int)vazou[4*i], (int)vazou[4*i+2]);
+    }
+    for(int i=5;i<11;i++){
+        senha.push_back(vazou[4*i]);
+    }
+    armazenamento.push_back(std::pair<std::map<char, std::pair<int, int>>, std::vector<char>>(codigo, senha));
     
 }
